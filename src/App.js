ad9d76b68a599de45825041a390be2db36680af7
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-04 01:32:39 +0600

    create components and 404 test

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Header from './components/Header/Header';
 import DeshBoard from './components/DeshBoard/DeshBoard';
 import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
         <Route path="/deshboard" element={<DeshBoard></DeshBoard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>I am not found components</h1>
+        </div>
+    );
+};
+
+export default NotFound;
